7948581f8391ae13c72fde9194666cbee260792f
Jack Pearson <jackrpearson@pm.me>
2020-09-25 12:45:21 -0700

    add ctags

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
+mkdir -p ~/.git_template/hooks
+ln -s ~/dotfiles/git_template/hooks/ctags ~/.git_template/hooks/ctags
