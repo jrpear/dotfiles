4dd9b5bf82a5ffaecac9b9700c4a01e8d0056cf6
Jack Pearson <jackrpearson@pm.me>
2020-09-24 18:35:43 -0700

    move to zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
---
+++ zshrc
@@ -0,0 +1,8 @@
+export ZSH="/home/jack/.oh-my-zsh"
+
+ZSH_THEME="robbyrussell"
+
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
