9a7396a6bd0bd4c55ed14a4c66d8d107e7fbb689
Jack Pearson <jackrpearson@pm.me>
2020-09-25 22:17:34 -0700

    add plugins to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vimrc.plugins ~/.vimrc.plugins
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
