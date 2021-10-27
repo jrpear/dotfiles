2132ba83381638ec5477b2d33e475e6c94995bde
Jack Pearson <jack@pearson.onl>
2021-10-27 15:49:13 -0700

    add pandas, scipy, matplotlib to ipython rc

--- ipython/profile_default/startup/rc.py
+++ ipython/profile_default/startup/rc.py
@@ -1,4 +1,8 @@
 from sympy import *
 import sympy.solvers as solvers
 init_printing()
+from math import radians, degrees
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
