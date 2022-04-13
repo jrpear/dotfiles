cbd2623338d1f49e7c6d76d625715d737a4b2112
Jack Pearson <jack@pearson.onl>
2022-04-13 16:10:47 -0700

    make install script fail more silently

--- install.sh
+++ install.sh
@@ -2,50 +2,52 @@
 
 # Bash
 
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ${PWD}/bashrc ~/.bashrc 2>/dev/null
+ln -s ${PWD}/bash_profile ~/.bash_profile 2>/dev/null
 
 # Core Vim
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/vimrc.plugins ~/.vimrc.plugins
+ln -s ${PWD}/vimrc ~/.vimrc 2>/dev/null
+ln -s ${PWD}/vimrc.plugins ~/.vimrc.plugins 2>/dev/null
 if [ ! -d ~/.vim ]
 then
   mkdir ~/.vim
-  ln -s ~/dotfiles/vim/after ~/.vim/after
-  ln -s ~/dotfiles/vim/plugin ~/.vim/plugin
+  ln -s ${PWD}/vim/after ~/.vim/after
+  ln -s ${PWD}/vim/plugin ~/.vim/plugin
 fi
 
 # Neovim
 
 mkdir -p ~/.config/nvim
-ln -s ~/dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ${PWD}/config/nvim/init.vim ~/.config/nvim/init.vim 2>/dev/null
 
 # Tmux
 
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ${PWD}/tmux.conf ~/.tmux.conf 2>/dev/null
 
 # Git
 
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ${PWD}/gitconfig ~/.gitconfig 2>/dev/null
+ln -s ${PWD}/gitignore ~/.gitignore 2>/dev/null
 mkdir -p ~/.git_template/hooks
-ln -s ~/dotfiles/git_template/hooks/ctags ~/.git_template/hooks/ctags
+ln -s ${PWD}/git_template/hooks/ctags ~/.git_template/hooks/ctags 2>/dev/null
 
 # IPython
 
 mkdir -p ~/.ipython/profile_default/startup
-ln -s ~/dotfiles/ipython/profile_default/ipython_config.py \
-  ~/.ipython/profile_default/ipython_config.py
-ln -s ~/dotfiles/ipython/profile_default/startup/rc.py ~/.ipython/profile_default/startup/rc.py
+ln -s ${PWD}/ipython/profile_default/ipython_config.py \
+  ~/.ipython/profile_default/ipython_config.py 2>/dev/null
+ln -s ${PWD}/ipython/profile_default/startup/rc.py \
+  ~/.ipython/profile_default/startup/rc.py 2>/dev/null
 
 # Sway
 
-mkdir -p ~/.config/sway
-ln -s ~/dotfiles/config/sway/config ~/.config/sway/config
-ln -s ~/dotfiles/config/swaylock/config ~/.config/swaylock/config
+mkdir -p ~/.config/sway{,lock}
+ln -s ${PWD}/config/sway/config ~/.config/sway/config 2>/dev/null
+ln -s ${PWD}/config/swaylock/config ~/.config/swaylock/config 2>/dev/null
 
 # VSCode
 
 mkdir -p ~/.config/Code/User
-ln -s ~/dotfiles/config/Code/User/settings.json ~/.config/Code/User/settings.json
+ln -s ${PWD}/config/Code/User/settings.json \
+  ~/.config/Code/User/settings.json 2>/dev/null
