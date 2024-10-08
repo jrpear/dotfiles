45e4955418d23250abd7028cde5b8e6fba7995e5
Jack Pearson <jack@pearson.onl>
2024-10-08 15:22:28 -0700

    move the installation of all files to the new method

--- install.sh
+++ install.sh
@@ -14,6 +14,20 @@ cp ${SCRIPT_DIR}/config/environment.d/enable-ibus.conf \
 repo_to_home=(
     "bashrc" ".bashrc"
     "bash_profile" ".bash_profile"
+    "vimrc" ".vimrc"
+    "vimrc.plugins" ".vimrc.plugins"
+    "vim" ".vim"
+    "vimrc" ".config/nvim/init.vim"
+    "vimrc.plugins" ".config/nvim/plugins.vim"
+    "vim" ".local/share/nvim/site"
+    "gdbinit" ".gdbinit"
+    "screenrc" ".screenrc"
+    "gitconfig" ".gitconfig"
+    "gitignore" ".gitignore"
+    "ipython/profile_default/ipython_config.py" ".ipython/profile_default/ipython_config.py"
+    "ipython/profile_default/startup/rc.py" ".ipython/profile_default/startup/rc.py"
+    "config/Code/User/settings.json" ".config/Code/User/settings.json"
+    "local/bin/windows.sh" ".local/bin/windows.sh"
 )
 
 while [ "${#repo_to_home[@]}" -gt 0 ]; do
@@ -32,50 +46,4 @@ while [ "${#repo_to_home[@]}" -gt 0 ]; do
     repo_to_home=("${repo_to_home[@]}")
 done
 
-# Core Vim
-
-ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc 2>/dev/null
-ln -s ${SCRIPT_DIR}/vimrc.plugins ${HOME}/.vimrc.plugins 2>/dev/null
-ln -s ${SCRIPT_DIR}/vim ${HOME}/.vim --no-target-directory 2>/dev/null
-
-# Neovim
-
-mkdir -p ${HOME}/.config/nvim
-mkdir -p ${HOME}/.local/share/nvim
-ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.config/nvim/init.vim 2>/dev/null
-ln -s ${SCRIPT_DIR}/vimrc.plugins ${HOME}/.config/nvim/plugins.vim 2>/dev/null
-ln -s ${SCRIPT_DIR}/vim ${HOME}/.local/share/nvim/site --no-target-directory 2>/dev/null
-
 git submodule update --init --recursive
-
-# GDB
-
-ln -s ${SCRIPT_DIR}/gdbinit ${HOME}/.gdbinit 2>/dev/null
-
-# Screen
-
-ln -s ${SCRIPT_DIR}/screenrc ${HOME}/.screenrc 2>/dev/null
-
-# Git
-
-ln -s ${SCRIPT_DIR}/gitconfig ${HOME}/.gitconfig 2>/dev/null
-ln -s ${SCRIPT_DIR}/gitignore ${HOME}/.gitignore 2>/dev/null
-
-# IPython
-
-mkdir -p ${HOME}/.ipython/profile_default/startup
-ln -s ${SCRIPT_DIR}/ipython/profile_default/ipython_config.py \
-  ${HOME}/.ipython/profile_default/ipython_config.py 2>/dev/null
-ln -s ${SCRIPT_DIR}/ipython/profile_default/startup/rc.py \
-  ${HOME}/.ipython/profile_default/startup/rc.py 2>/dev/null
-
-# VSCode
-
-mkdir -p ${HOME}/.config/Code/User
-ln -s ${SCRIPT_DIR}/config/Code/User/settings.json \
-  ${HOME}/.config/Code/User/settings.json 2>/dev/null
-
-# Windows VM management script
-
-mkdir -p ${HOME}/.local/bin
-ln -s ${SCRIPT_DIR}/local/bin/windows.sh ${HOME}/.local/bin/windows.sh 2>/dev/null
